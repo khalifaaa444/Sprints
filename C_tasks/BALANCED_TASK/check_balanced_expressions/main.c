20b122298969de92171c68f445150cfc017b5a45
khalifaaa444 <116544385+khalifaaa444@users.noreply.github.com>
2023-04-29 18:00:21 +0200

    Update main.c

--- C_tasks/BALANCED_TASK/check_balanced_expressions/main.c
+++ C_tasks/BALANCED_TASK/check_balanced_expressions/main.c
@@ -1,190 +1,222 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
 
 #define STACK_SIZE 10
 
+typedef unsigned char uint8_t;
+typedef signed char  int8_t;
 
-typedef struct stack
-{
-    uint8_t elements[STACK_SIZE];
-    int8_t top;
-} ST_stack_t;
+typedef struct stack {
+uint8_t elements[STACK_SIZE];
+int8_t top;
+}ST_stack_t; // Type
 
-void createEmptyStack(ST_stack_t* stack);
-
-
-int8_t push(ST_stack_t* stack, uint8_t data);
-
-int8_t pop(ST_stack_t* stack, uint8_t* data);
-
-int8_t printStack(ST_stack_t* stack);
-
-int8_t getStackTop(ST_stack_t* stack, uint8_t* topData);
-
-int8_t isBalancedParanthethes(uint8_t* expression);
-
-
-
-
-int8_t i;
-
-
+void createEmptyStack(ST_stack_t *stack);
+int8_t push(ST_stack_t *stack, uint8_t data);
+int8_t pop(ST_stack_t *stack, uint8_t *data);
+int8_t printStack(ST_stack_t *stack);
+int8_t getStackTop(ST_stack_t *stack, uint8_t *topData);
+int8_t isFull(ST_stack_t *stack);
+int8_t isEmpty(ST_stack_t *stack);
+int8_t isBalancedParanthethes(uint8_t *expression);
 int main()
 {
-    int8_t state = 0;
-   // uint8_t array[10] = "{(2+3)}";
-   uint8_t array[10];
-  while(1)
-  {
-
-
-   printf("Enter your string :  ");
-   scanf("%s",array);
-    state=isBalancedParanthethes(array);
-    switch (state)
+    char st[11];
+while(1)
+{
+    scanf("%s",st);
+    int8_t result = isBalancedParanthethes(st);
+    if(result == 0)
     {
-    case 0:
-        printf("it is balanced\n",array);
-        break;
-    case -1:
-        printf("It is not balanced\n",array);
-        break;
-    case -2:
-        printf("It doesn't include paranthethes\n",array);
-        break;
+        printf("balanced\n");
     }
-    printf("press 'y' if want to try another expression: ");
-    char ch;
-
-    scanf(" %c",&ch);
-
-    if(ch != 'y')
+    else if(result == -1)
     {
-        break;
+        printf("unbalanced\n");
     }
-  }
+    else if(result == -2)
+    {
+        printf("the neither of paranthethes is used\n");
+    }
+}
+
     return 0;
 }
 
-
-
-
-void createEmptyStack(ST_stack_t* stack)
+void createEmptyStack(ST_stack_t *stack)
 {
-    stack -> top = -1;
-    for ( i = 0; i < STACK_SIZE; i++)
+    int i=0;
+
+    for(i=0;i<STACK_SIZE;i++)
     {
         stack->elements[i] = 0;
     }
-}
-
-
-int8_t push(ST_stack_t* stack, uint8_t data)
-{
-
-    if (stack->top == (STACK_SIZE-1))
-    {
-        return -1;
-    }
-    stack->top++;
-    stack->elements[stack->top] = data;
-    return 0;
-}
-
-int8_t pop(ST_stack_t* stack, uint8_t* data)
-{
-    if (stack->top == -1)
-    {
-        return -2;
-    }
-    *data = stack->elements[stack->top];
-    stack->top--;
-    return 0;
+    stack->top = -1;
 
 }
 
-int8_t printStack(ST_stack_t* stack)
+int8_t push(ST_stack_t *stack, uint8_t data)
 {
-    if (stack->top == (STACK_SIZE - 1))
+    int8_t flag=0;
+   if(stack->top == STACK_SIZE -1 )
+   {
+       flag=-1;
+   }
+   else
+   {
+       stack->top++;
+       stack->elements[stack->top]=data;
+
+   }
+   return flag;
+}
+
+
+int8_t pop(ST_stack_t *stack, uint8_t *data)
+{
+    int8_t flag =0;
+    if(stack->top == -1)
     {
-        return -1;
+        flag =-2;
     }
-    else if (stack->top == -1)
+
+    else
     {
-        return -2;
+        *data = stack->elements[(stack->top)];
+        stack->top--;
+        //printf("%i",stack->top);
+    }
+    return flag;
+}
+
+int8_t printStack(ST_stack_t *stack)
+{
+    int8_t flag=0;
+    int8_t temp = stack->top;
+
+    if(temp == STACK_SIZE -1)
+    {
+        flag = -2;
+        while(temp != -1)
+        {
+            temp--;
+            printf("%c\n",stack->elements[temp+1]);
+        }
+    }
+    else if(temp == -1)
+    {
+        flag = -1;
     }
     else
     {
-        for ( i = stack->top; i >= 0; i--)
+        while(temp != -1)
         {
-            printf("%c ", stack->elements[i]);
+            temp--;
+            printf("%c\n",stack->elements[temp+1]);
         }
-        printf("\n");
-        return 0;
     }
+
+    return flag;
+
 }
 
-int8_t getStackTop(ST_stack_t* stack, uint8_t* topData)
+
+int8_t getStackTop(ST_stack_t *stack, uint8_t *topData)
 {
-    if (stack->top == -1)
+    int8_t flag;
+    if(stack->top == -1)
     {
-        return -2;
+        flag =-2;
     }
-    *topData = stack->elements[stack->top];
-    return 0;
+    else
+    {
+        *topData=stack->elements[stack->top];
+        flag=0;
+    }
+
+    return flag;
+
+
 }
 
-int8_t isEmpty(ST_stack_t* stack)
+
+int8_t isFull(ST_stack_t *stack)
 {
-    if (stack->top == -1)
+    int8_t flag=0;
+
+    if(stack->top == STACK_SIZE -1)
     {
-        return -2;
+        flag=-1;
     }
-    return 0;
+    return flag;
+
 }
 
-int8_t isBalancedParanthethes(uint8_t* expression)
+int8_t isEmpty(ST_stack_t *stack)
 {
-    ST_stack_t MyStack;
-    createEmptyStack(&MyStack);
-    uint8_t temp = 0;
+    int8_t flag =0;
 
-    for ( i = 0; i < strlen(expression); i++)
+    if(stack->top == -1)
     {
+        flag=-2;
+    }
 
-        getStackTop(&MyStack, &temp);
+    return flag;
 
-        if (expression[i] == '{' || expression[i] == '(')
+}
+
+
+
+int8_t isBalancedParanthethes(uint8_t *expression)
+{
+    uint8_t count1=0,count2=0,i=0;
+    int8_t flag=0;
+
+    uint8_t len = strlen(expression);
+    for(i=0;i<len;i++)
+    {
+        if((expression[i]=='[')||(expression[i]==']')||(expression[i]=='<')||(expression[i]=='>'))
         {
-            push(&MyStack, expression[i]);
+            flag = -2;
+            break;
         }
-        else if (expression[i] == ')' || expression[i] == '}')
-        {
-            if (MyStack.top == -1)
+        if(expression[i]=='{')
             {
-                return -1;
+                count1++;
             }
-            pop(&MyStack, &temp);
-            if ((expression[i] == '}' && temp != '{') || (expression[i] == ')' && temp != '('))
+        if(expression[i]=='}')
             {
-                return -1;
+                count1--;
             }
-
+        if(expression[i]=='(')
+            {
+                count2++;
+            }
+        if(expression[i]==')')
+            {
+                count2--;
+            }
+        if((count1<0)||(count2<0))
+        {
+            flag=-1;
+            break;
         }
     }
-    if (MyStack.top != -1)
+    if(i == (len))
     {
-        return -1;
+        if((count1>0)||(count2>0))
+        {
+            flag=-1;
+        }
     }
 
-    else if (temp == '\0')
-    {
-        return -2;
-    }
+    //printf("%i\n",flag);
+    return flag;
 
-    return 0;
+
 
 }
+
+
 
