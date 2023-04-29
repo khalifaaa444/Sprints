9eecc781a6e33dd767f73518e1483bccf4f85215
khalifaaa444 <116544385+khalifaaa444@users.noreply.github.com>
2023-04-29 17:59:37 +0200

    Update main.c

--- C_tasks/BALANCED_TASK/Algoritm/main.c
+++ C_tasks/BALANCED_TASK/Algoritm/main.c
@@ -1,191 +1,226 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
+
 
 #define STACK_SIZE 10
 
+typedef unsigned char uint8_t;
+typedef signed char  int8_t;
 
 typedef struct stack {
-	uint8_t elements[STACK_SIZE];
-	int8_t top;
-}ST_stack_t;
+uint8_t elements[STACK_SIZE];
+int8_t top;
+}ST_stack_t; // Type
 
-int8_t isBalancedParanthethes(uint8_t* expression);
-int8_t isEmpty(ST_stack_t* stack);
-int8_t getStackTop(ST_stack_t* stack, uint8_t* topData);
-int8_t printStack(ST_stack_t* stack);
-int8_t pop(ST_stack_t* stack, uint8_t* data);
-int8_t push(ST_stack_t* stack, uint8_t data);
-void createEmptyStack(ST_stack_t* stack);
-
-int8_t i;
-
-int main() {
-
-	uint8_t ch;
-
-	ST_stack_t stack;
-
-	createEmptyStack(&stack);
-    printf("Enter 5 charecters : ");
-    char inp[10];
-    for(i=0;i<5;i++)
+void createEmptyStack(ST_stack_t *stack);
+int8_t push(ST_stack_t *stack, uint8_t data);
+int8_t pop(ST_stack_t *stack, uint8_t *data);
+int8_t printStack(ST_stack_t *stack);
+int8_t getStackTop(ST_stack_t *stack, uint8_t *topData);
+int8_t isFull(ST_stack_t *stack);
+int8_t isEmpty(ST_stack_t *stack);
+int main()
+{
+     ST_stack_t NewST;
+     createEmptyStack(&NewST);
+     uint8_t choice;
+     int8_t result;
+     uint8_t data;
+     //int data_2;
+    while(1)
     {
-        scanf(" %c",&inp[i]);
+        printf("choose your order :  1. push   2. pop   3. print topStack   4. print all stack    :- \t \t ");
+        scanf(" %c",&choice);
+        if(choice == '1' )
+        {
+            printf("enter Data :   ");
+            fflush(stdin);fflush(stdout);
+            scanf(" %c",&data);
+            fflush(stdin);fflush(stdout);
+            result = push(&NewST,data);
+            if(result == 0)
+            {
+                printf("data: '%c' is pushed successfully\n",data);
+            }
+            else
+            {
+                printf("Stack is full\n");
+            }
+        }
+        else if(choice == '2')
+        {
+
+            result = pop(&NewST,&data);
+            if(result == 0)
+            {
+                printf("data: '%c'  is poped successfully\n",data);
+            }
+            else
+            {
+                printf("Stack is empty\n");
+            }
+
+        }
+
+        else if(choice == '3')
+        {
+            result = getStackTop(&NewST,&data);
+            if(result == 0)
+            {
+                printf("data: '%c' is in top of stack \n",data);
+            }
+            else
+            {
+                printf("Stack is empty\n");
+            }
+        }
+
+        else if(choice == '4')
+        {
+            result = printStack(&NewST);
+            if(result == 0)
+            {
+                printf("Note : Stack isn't full\n");
+
+            }
+            else if(result == -2)
+            {
+                printf("Note :Stack is full \n");
+            }
+            else
+            {
+                printf("Stack is empty\n");
+            }
+        }
+
+        else
+        {
+            printf("\nTry again .... \n");
+        }
     }
 
-	for(i=0;i<5;i++)
+
+    return 0;
+}
+
+void createEmptyStack(ST_stack_t *stack)
+{
+    int i=0;
+
+    for(i=0;i<STACK_SIZE;i++)
     {
-        push(&stack, inp[i]);
+        stack->elements[i] = 0;
+    }
+    stack->top = -1;
+
+}
+
+int8_t push(ST_stack_t *stack, uint8_t data)
+{
+    int8_t flag=0;
+   if(stack->top == STACK_SIZE -1 )
+   {
+       flag=-1;
+   }
+   else
+   {
+       stack->top++;
+       stack->elements[stack->top]=data;
+
+   }
+   return flag;
+}
+
+
+int8_t pop(ST_stack_t *stack, uint8_t *data)
+{
+    int8_t flag =0;
+    if(stack->top == -1)
+    {
+        flag =-2;
     }
 
-	printStack(&stack);
-
-	pop(&stack, &ch);
-
-	printf("Top after popping one character: \n");
-	getStackTop(&stack, &ch);
-
-	printf("Stack after popping one character: \n");
-    printStack(&stack);
-
-
-    printf("Enter 7 charecters : ");
-
-    for(i=0;i<7;i++)
+    else
     {
-        scanf(" %c",&inp[i]);
+        *data = stack->elements[(stack->top)];
+        stack->top--;
+        //printf("%i",stack->top);
+    }
+    return flag;
+}
+
+int8_t printStack(ST_stack_t *stack)
+{
+    int8_t flag=0;
+    int8_t temp = stack->top;
+
+    if(temp == STACK_SIZE -1)
+    {
+        flag = -2;
+        while(temp != -1)
+        {
+            temp--;
+            printf("%c\n",stack->elements[temp+1]);
+        }
+    }
+    else if(temp == -1)
+    {
+        flag = -1;
+    }
+    else
+    {
+        while(temp != -1)
+        {
+            temp--;
+            printf("%c\n",stack->elements[temp+1]);
+        }
     }
 
-	for(i=0;i<7;i++)
+    return flag;
+
+}
+
+
+int8_t getStackTop(ST_stack_t *stack, uint8_t *topData)
+{
+    int8_t flag;
+    if(stack->top == -1)
     {
-        push(&stack, inp[i]);
+        flag =-2;
+    }
+    else
+    {
+        *topData=stack->elements[stack->top];
+        flag=0;
     }
 
+    return flag;
 
 
-	printf("Top after pushing 7 characters:- \n");
-	getStackTop(&stack, &ch);
-
-	printf("Stack after pushing 7 characters:- \n");
-	printStack(&stack);
-
-	for ( i = 0; i < 4; i++) {
-		pop(&stack, &ch);
-	}
-	printf("Top after popping 4 characters:- \n");
-	getStackTop(&stack, &ch);
-
-	printStack(&stack);
-
-	for ( i = 0; i < 7; i++) {
-		pop(&stack, &ch);
-	}
-	printf("Top after popping 7 characters:- \n");
-	getStackTop(&stack, &ch);
-
-	printStack(&stack);
-
-	return 0;
 }
 
 
+int8_t isFull(ST_stack_t *stack)
+{
+    int8_t flag=0;
 
-
-
-void createEmptyStack(ST_stack_t* stack) {
-	stack -> top = -1;
-	for ( i = 0; i < STACK_SIZE; i++) {
-		stack->elements[i] = 0;
-	}
-}
-
-
-int8_t push(ST_stack_t* stack, uint8_t data) {
-
-	if (stack->top == (STACK_SIZE-1)) {
-		return -1;
-	}
-	stack->top++;
-	stack->elements[stack->top] = data;
-	return 0;
-}
-
-int8_t pop(ST_stack_t* stack, uint8_t* data) {
-	if (stack->top == -1) {
-		return -2;
-	}
-	*data = stack->elements[stack->top];
-	stack->top--;
-	return 0;
+    if(stack->top == STACK_SIZE -1)
+    {
+        flag=-1;
+    }
+    return flag;
 
 }
 
-int8_t printStack(ST_stack_t* stack) {
-	if (stack->top == (STACK_SIZE - 1)) {
-		printf("Stack is FULL!\n");
-		return -1;
-	}
-	else if (stack->top == -1) {
-		printf("Stack is EMPTY!\n");
-		return -2;
-	}
-	else {
-		for ( i = stack->top; i >= 0; i--) {
-			printf("%c ", stack->elements[i]);
-		}
-		printf("\n");
-		return 0;
-	}
+int8_t isEmpty(ST_stack_t *stack)
+{
+    int8_t flag =0;
+
+    if(stack->top == -1)
+    {
+        flag=-2;
+    }
+
+    return flag;
+
 }
-
-
-int8_t getStackTop(ST_stack_t* stack, uint8_t* topData) {
-	if (stack->top == -1) {
-		return -2;
-		printf("Stack is EMPTY!\n");
-	}
-	*topData = stack->elements[stack->top];
-	printf(" %c\n", *topData);
-	return 0;
-}
-
-int8_t isEmpty(ST_stack_t* stack) {
-	if (stack->top == -1) {
-		return -2;
-	}
-	return 0;
-}
-
-int8_t isBalancedParanthethes(uint8_t* expression) {
-	ST_stack_t MyStack;
-	createEmptyStack(&MyStack);
-
-
-	for ( i = 0; i < strlen(expression); i++) {
-		uint8_t temp = 0;
-		getStackTop(&MyStack, &temp);
-
-		if (expression[i] == '{' || expression[i] == '(') {
-			push(&MyStack, expression[i]);
-		}
-		else if (expression[i] == ')' || expression[i] == '}') {
-			if (MyStack.top == -1) {
-				return -1;
-			}
-
-			if ((expression[i] == '}' && temp == '{') || (expression[i] == ')' && temp == '(')) {
-				pop(&MyStack, &temp);
-			}
-			else {
-				return -1;
-			}
-		}
-	}
-	if (MyStack.top == -1) {
-	return -2;
-	}
-	return 0;
-}
-
