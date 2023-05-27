adb5de9fd52be8ab1c368c876fc9fc3da28d0f80
khalifaaa444 <116544385+khalifaaa444@users.noreply.github.com>
2023-05-27 14:24:18 +0300

    Create uart_service.h

---
+++ SW_design/dynamic_design/service/uart_service.h
@@ -0,0 +1,22 @@
+#ifndef UART_SERVICES_H_
+#define UART_SERVICES_H_
+
+#include "uart.h"
+
+
+#define END_SEND           0x0D
+#define INVALID_NUM        0x00
+
+
+/*******************************************************Transmitter************************************************************************************************/
+void uart_transmitString(u8*str);
+void uart_transmitString_Interrupt(u8*str);
+void uart_transmitNumber_MCU(u32 num);
+void uart_transmitNumber_MCU_Interrupt(u32* num);
+
+
+/*******************************************************Receiver*********************************************************************************************/
+void uart_recieverString(u8*str);
+void uart_recieverString_Interrupt(u8*str);
+void uart_recieveNumber_MCU(u32* num);
+void uart_recieveNumber_MCU_Interrupt(u32* num);
