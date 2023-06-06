a4533dfbbb314cb6c9c50d53ebaf6ab66c22864f
khalifaaa444 <116544385+khalifaaa444@users.noreply.github.com>
2023-06-07 01:24:15 +0300

    Add files via upload

---
+++ MISRA_RULES_TASKS/LED_SEQ1_MISRA/LED_SEQ1/LED_SEQ1/Debug/mcal/dio/dio.d
@@ -0,0 +1,13 @@
+mcal/dio/dio.d mcal/dio/dio.o: ../mcal/dio/dio.c ../mcal/dio/dio.h \
+ ../mcal/dio/../../common/std_types.h ../mcal/dio/../../common/memmap.h \
+ ../mcal/dio/../../common/std_types.h ../mcal/dio/../../common/bit_math.h
+
+../mcal/dio/dio.h:
+
+../mcal/dio/../../common/std_types.h:
+
+../mcal/dio/../../common/memmap.h:
+
+../mcal/dio/../../common/std_types.h:
+
+../mcal/dio/../../common/bit_math.h:
